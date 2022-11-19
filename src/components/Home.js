5b5a032d45cc86bb2e866f826ed1df4a1f890647
shivam <shivam.sharma@unotechsoft.com>
2022-11-19 12:51:33 +0530

    added some change to test home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,27 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <h1>Home1</h1>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate("/counter");
+        }}>
+        Counter
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate("/form");
+        }}>
+        form
+      </Button>
+    </>
+  );
 };
 
 export default Home;
